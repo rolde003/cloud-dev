20ef6c4ae7dadf0992149e540536b6d6f80394a1
rolde <126878789+rolde003@users.noreply.github.com>
2024-11-29 14:27:15 +0100

    app.js

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-const sassMiddleware = require('node-sass-middleware')
 
 const indexRouter = require('./routes/index')
-// const usersRouter = require('./routes/users')
+const usersRouter = require('./routes/users')
 
 const app = express()
 
@@ -18,16 +17,10 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true,
-}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-// app.use('/users', usersRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
